c3ca66497330556419bd81dcf6a91cb5c9a29a78
risktoollib <pcote@ualberta.ca>
2022-01-15 17:53:23 -0700

    2022 01 15

--- dev/01_start.R
+++ dev/01_start.R
@@ -21,10 +21,10 @@
 golem::fill_desc(
   pkg_name = "RTLappBonds", # The Name of the package containing the App 
   pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_description = "Demonstrated fixed income risk at the portfolio level.", # The Description of the package containing the App 
+  author_first_name = "Philippe", # Your First Name
+  author_last_name = "Cote", # Your Last Name
+  author_email = "pcote@ualberta.ca", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional) 
 )     
 
@@ -33,14 +33,15 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( "Golem User" )  # You can set another license here
-usethis::use_readme_rmd( open = FALSE )
-usethis::use_code_of_conduct()
+usethis::use_mit_license( "Philippe Cote" )  # You can set another license here
+usethis::use_readme_rmd( open = TRUE )
+#usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
-usethis::use_news_md( open = FALSE )
+#usethis::use_news_md( open = FALSE )
 
 ## Use git ----
-usethis::use_git()
+usethis::use_github()
+#usethis::use_git()
 
 ## Init Testing Infrastructure ----
 ## Create a template for tests
@@ -51,8 +52,8 @@ golem::use_recommended_deps()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file. 
-golem::remove_favicon()
+#golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+#golem::remove_favicon()
 
 ## Add helper functions ----
 golem::use_utils_ui()
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(RTLappBonds)
+
+test_check("RTLappBonds")
