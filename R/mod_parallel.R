5186e284c3ea9ca6bfde1b99e2e87f63073fe67c
risktoollib <pcote@ualberta.ca>
2023-03-26 10:52:21 -0600

    cpp working

--- R/mod_parallel.R
+++ R/mod_parallel.R
@@ -39,39 +39,110 @@ mod_parallel_server <- function(id, r){
       s = 0 # initial ytm
 
       StepSize <- shiny::reactive(as.numeric(input$stepSize) / 10000)
-      #browser()
+      
+      browser()
+      
+      # junk <-
+      #   dplyr::tibble(
+      #     Yield = round(seq(s - 0.03, s + 0.03, StepSize()), 4),
+      #     Price = 0,
+      #     x = tidyr::nest(.data = r$port, data = dplyr::everything())
+      #   ) %>%
+      #   dplyr::mutate(Price = purrr::pmap(.l = list(x = x$data, shockpar = Yield), .f = portfolioMTM),
+      #                 PriceMinus = purrr::pmap(.l = list(x = x$data, shockpar = Yield - StepSize()), .f = portfolioMTM),
+      #                 PricePlus = purrr::pmap(.l = list(x = x$data, shockpar = Yield + StepSize()), .f = portfolioMTM)
+      #                 ) %>%
+      #   dplyr::mutate(Price = purrr::map_dbl(Price, .f = as.numeric),
+      #                PriceMinus = purrr::map_dbl(PriceMinus, .f = as.numeric),
+      #                PricePlus = purrr::map_dbl(PricePlus, .f = as.numeric),
+      #                Delta = (PricePlus - PriceMinus) / (2 * StepSize()) / 10000,
+      #                Gamma = 0.5 * ((PricePlus - 2 * Price + PriceMinus) /  StepSize() ^ 2) / 10000 ^ 2
+      #                )
+      # 
+      # Price.Local <- as.numeric(dplyr::filter(junk,Yield == 0)$Price)
+      # DeltaLocal <- as.numeric(dplyr::filter(junk,Yield == 0)$Delta)
+      # GammaLocal <- as.numeric(dplyr::filter(junk,Yield == 0)$Gamma)
+      # 
+      # sens <- junk %>%
+      #   dplyr::mutate(
+      #     DeltaApprox = Price.Local + DeltaLocal * (Yield) * 10000,
+      #     ActualPL = Price - Price.Local,
+      #     DeltaPL = DeltaLocal * (Yield) * 10000,
+      #     GammaPL = GammaLocal * ((Yield) * 10000) ^ 2,
+      #     DeltaGammaPL = DeltaPL + GammaPL,
+      #     UnexplainedPL = Price - (Price.Local + DeltaPL + GammaPL)
+      #   )
+      
 
-      junk <-
-        dplyr::tibble(
-          Yield = round(seq(s - 0.03, s + 0.03, StepSize()), 4),
-          Price = 0,
-          x = tidyr::nest(.data = r$port, data = dplyr::everything())
-        ) %>%
-        dplyr::mutate(Price = purrr::pmap(.l = list(x = x$data, shockpar = Yield), .f = portfolioMTM),
-                      PriceMinus = purrr::pmap(.l = list(x = x$data, shockpar = Yield - StepSize()), .f = portfolioMTM),
-                      PricePlus = purrr::pmap(.l = list(x = x$data, shockpar = Yield + StepSize()), .f = portfolioMTM)
-                      ) %>%
-        dplyr::mutate(Price = purrr::map_dbl(Price, .f = as.numeric),
-                     PriceMinus = purrr::map_dbl(PriceMinus, .f = as.numeric),
-                     PricePlus = purrr::map_dbl(PricePlus, .f = as.numeric),
-                     Delta = (PricePlus - PriceMinus) / (2 * StepSize()) / 10000,
-                     Gamma = 0.5 * ((PricePlus - 2 * Price + PriceMinus) /  StepSize() ^ 2) / 10000 ^ 2
-                     )
+# cpp ---------------------------------------------------------------------
 
-      Price.Local <- as.numeric(dplyr::filter(junk,Yield == 0)$Price)
-      DeltaLocal <- as.numeric(dplyr::filter(junk,Yield == 0)$Delta)
-      GammaLocal <- as.numeric(dplyr::filter(junk,Yield == 0)$Gamma)
-
-      sens <- junk %>%
+      for (i in 1:3) {
+        x <-
+          dplyr::tibble(
+            Notional = r$port$Notional[i],
+            Maturity = r$port$Maturity[i],
+            Coupon = r$port$Coupon[i],
+            YTM = r$port$YTM[i],
+            Shock = r$port$Shock[i],
+            Yield = round(seq(s - 0.03, s + 0.03, StepSize()), 4),
+            Duration = 0,
+            Price = 0,
+            PriceMinus = 0,
+            PricePlus = 0,
+            Delta = 0,
+            Gamma = 0,
+            DeltaApprox = 0,
+            ActualPL = 0,
+            DeltaPL = 0,
+            GammaPL = 0,
+            DeltaGammaPL = 0,
+            UnexplainedPL = 0
+          )
+        if (i == 1) {sens <- x} else {
+          sens <- rbind(sens,x)
+        }
+      }
+        
+      Rcpp::sourceCpp("./src/rcppPortParallel.cpp")
+      sens <- rcppPortParallel(x = base::as.matrix(sens), stepSize = StepSize()) %>% 
+        dplyr::as_tibble() 
+      
+      local <- dplyr::filter(sens,Yield == 0)
+    
+      sens <- sens %>%
         dplyr::mutate(
-          DeltaApprox = Price.Local + DeltaLocal * (Yield) * 10000,
-          ActualPL = Price - Price.Local,
-          DeltaPL = DeltaLocal * (Yield) * 10000,
-          GammaPL = GammaLocal * ((Yield) * 10000) ^ 2,
+          PriceLocal = dplyr::case_when(
+            (Notional == r$port$Notional[1] & Maturity == r$port$Maturity[1]) ~ local$Price[1],
+            (Notional == r$port$Notional[2] & Maturity == r$port$Maturity[2]) ~ local$Price[2],
+            (Notional == r$port$Notional[3] & Maturity == r$port$Maturity[3]) ~ local$Price[3],
+            TRUE ~ 0
+          ),
+          DeltaLocal = dplyr::case_when(
+            (Notional == r$port$Notional[1] & Maturity == r$port$Maturity[1]) ~ local$Delta[1],
+            (Notional == r$port$Notional[2] & Maturity == r$port$Maturity[2]) ~ local$Delta[2],
+            (Notional == r$port$Notional[3] & Maturity == r$port$Maturity[3]) ~ local$Delta[3],
+            TRUE ~ 0
+          ),
+          GammaLocal = dplyr::case_when(
+            (Notional == r$port$Notional[1] & Maturity == r$port$Maturity[1]) ~ local$Gamma[1],
+            (Notional == r$port$Notional[2] & Maturity == r$port$Maturity[2]) ~ local$Gamma[2],
+            (Notional == r$port$Notional[3] & Maturity == r$port$Maturity[3]) ~ local$Gamma[3],
+            TRUE ~ 0
+          ),
+          DeltaApprox = PriceLocal + DeltaLocal * (Yield / StepSize()),
+          ActualPL = Price - PriceLocal,
+          DeltaPL = DeltaLocal * (Yield / StepSize()),
+          GammaPL = GammaLocal * ((Yield / StepSize())) ^ 2,
           DeltaGammaPL = DeltaPL + GammaPL,
-          UnexplainedPL = Price - (Price.Local + DeltaPL + GammaPL)
+          UnexplainedPL = Price - (PriceLocal + DeltaPL + GammaPL)
         )
-
+      
+      sens %>% 
+        dplyr::select(-1:-5) %>% 
+        dplyr::group_by(Yield) %>% 
+        dplyr::summarise_all(.funs = list(sum))
+      
+        
       sens %>%
         plotly::plot_ly(x = ~Yield, y = ~ActualPL, name = "Actual PL", type = 'scatter', mode = 'lines', line = list(dash = "solid")) %>%
         #plotly::plot_ly(x = ~Yield, y = ~Price, name = "Portfolio Value", type = 'scatter', mode = 'lines') %>%
---
+++ src/rcppPortParallel.cpp
@@ -0,0 +1,71 @@
+#include <Rcpp.h>
+#include <vector>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericVector rcppPortParallel(NumericMatrix x, double stepSize) {
+
+  for (int i = 0; i < x.nrow(); i++) {
+    // pricing and duration
+    double m = 2;
+    double periods = (x(i,1) * m) + 1;
+    double ytm = x(i,3) + x(i,5); // same as coupons for CMTs
+    double coupon = x(i,2);
+
+    NumericVector tyears(periods);
+    for (int j = 1; j < periods; j++) {tyears[j] = tyears[j-1] + 1/m;}
+    NumericVector cf = Rcpp::rep(coupon * 100 / m, periods);
+    cf[0] = 0;
+    cf[periods-1] = 100 + cf[periods-1];
+    NumericVector disc_factor(periods);
+    for (int j = 0; j < periods; j++) {
+      disc_factor[j] = 1 / pow(1 + ytm / m, tyears[j] * m);
+      }
+    double pv = 0;
+    for (int j = 0; j < periods; j++) {pv += cf[j] * disc_factor[j];}
+    double duration = 0;
+    for (int j = 0; j < periods; j++) {duration += cf[j] * disc_factor[j] * tyears[j]/ pv;}
+    x(i,7) = pv;
+    x(i,6) = duration;
+
+    // delta/gamma (expo)
+      // down shift
+    double ytmDown = ytm - stepSize;
+    for (int j = 0; j < periods; j++) {
+      disc_factor[j] = 1 / pow(1 + ytmDown / m, tyears[j] * m);
+    }
+    double pv01Down = 0;
+    for (int j = 0; j < periods; j++) {pv01Down += cf[j] * disc_factor[j];}
+    x(i,8) = pv01Down;
+
+      // up shift
+    double ytmUp = ytm + stepSize;
+    for (int j = 0; j < periods; j++) {
+      disc_factor[j] = 1 / pow(1 + ytmUp / m, tyears[j] * m);
+    }
+    double pv01Up = 0;
+    for (int j = 0; j < periods; j++) {pv01Up += cf[j] * disc_factor[j];}
+    x(i,9) = pv01Up;
+    // delta
+    x(i,10) = (pv01Up - pv01Down) / 2;
+    // gamma
+    x(i,11) = 0.5 * ((pv01Up - 2 * pv + pv01Down) / (stepSize * stepSize)) / (10000 * 10000);
+  }
+
+return x;
+
+  //IntegerVector a = Rcpp::as<IntegerVector>(periods);
+  //IntegerVector x = Rcpp::seq(0,Rcpp::as<IntegerVector>(periods));
+  //NumericVector y = Rcpp::as<NumericVector>(x);
+
+  // for (int i = 0; i < ytm.size(); i++) {
+  //   NumericVector tyears = seq(0, T2M[i]);
+  //   tyears = tyears * m[i]
+  //   NumericVector cf = rep(C[i] * 100 / m[i], T2M[i] * m[i]);
+  //   NumericVector t_periods = m[i] * tyears;
+  //   cf = ifelse(tyears == T2M[i],C[i] * 100 / m[i] + 100,cf);
+  //   NumericVector disc_factor = 1 / pow(1 + ytm[i] / m[i], t_periods);
+  //   NumericVector pv = cf * disc_factor;
+  //   price[i] = sum(pv);
+  // }
+}
